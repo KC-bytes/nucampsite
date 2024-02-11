4d9b3bb469594ed7e4458677a1b09b4cdd13d6cb
HexedBanana <113743348+KC-bytes@users.noreply.github.com>
2024-02-10 19:12:18 -0800

    Update LoadingComponent.js

--- components/LoadingComponent.js
+++ components/LoadingComponent.js
@@ -1,5 +1,4 @@
-import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
-
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
 function Loading() {
     return (
         <View style={styles.loadingView}>
